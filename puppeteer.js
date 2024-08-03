84ba32ca010d29b24d7d59dd500558455a1016ae
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-08-03 12:32:48 +0300

    fix: token

--- puppeteer.js
+++ puppeteer.js
@@ -1,16 +1,26 @@
 // import puppeteer from "puppeteer-core";
 import fetch from "node-fetch";
 // const executablePath = process.env.EXECUTABLE_PATH || "/usr/bin/google-chrome";
-
+const token = process.env.BROWSERLESS_TOKEN;
+const proxyUrl = `https://browserless-production-f7b8.up.railway.app/content?token=${token}`;
 export async function getPageContent(url) {
   try {
-    const proxyUrl =
-      "https://browserless-production-f7b8.up.railway.app/content?token=wwoqW3Cp5qDBDCCGlL7qcUOwvQbaoBJvMPc7Ug2ks6JDaTJb";
-
     const req = await fetch(proxyUrl, {
       method: "POST",
       body: JSON.stringify({
         url,
+        rejectResourceTypes: [
+          "image",
+          "stylesheet",
+          "font",
+          "texttrack",
+          "script",
+        ],
+        rejectRequestPattern: ["/^.*\\.(css)"],
+        gotoOptions: {
+          waitUntil: "domcontentloaded",
+        },
+        setJavaScriptEnabled: false,
       }),
       headers: {
         "Content-Type": "application/json",
