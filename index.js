c56e21eee97e03e4bf6f3fbf2582f8bd72e5829b
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-08-04 02:09:33 +0300

    disable webhook

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 4000;
 app.use("/feed", getFeed);
 app.use("/content", getContent);
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use("/webhook", (req, res) => {
-  console.log("webhook", req.body);
-  res.send({
-    message: "ok",
-  });
-});
+// app.use("/webhook", (req, res) => {
+//   console.log("webhook", req.body);
+//   res.send({
+//     message: "ok",
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`live on ${PORT}`);
