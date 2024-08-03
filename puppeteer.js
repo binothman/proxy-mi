14e885037d37b401c7831850aa284a7b68328dc7
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-08-03 12:09:25 +0300

    remove puputer

--- puppeteer.js
+++ puppeteer.js
@@ -1,57 +1,23 @@
-import puppeteer from "puppeteer-core";
-
+// import puppeteer from "puppeteer-core";
+import fetch from "node-fetch";
 // const executablePath = process.env.EXECUTABLE_PATH || "/usr/bin/google-chrome";
 
 export async function getPageContent(url) {
   try {
-    const browser = await puppeteer.connect({
-      browserWSEndpoint:
-        "wss://browserless-production-f7b8.up.railway.app?token=wwoqW3Cp5qDBDCCGlL7qcUOwvQbaoBJvMPc7Ug2ks6JDaTJb",
-    });
-    const [page] = await browser.pages();
+    const proxyUrl =
+      "https://browserless-production-f7b8.up.railway.app/content?token=wwoqW3Cp5qDBDCCGlL7qcUOwvQbaoBJvMPc7Ug2ks6JDaTJb";
 
-    const client = await page.target().createCDPSession();
-    await client.send("Network.disable");
-
-    await page.setRequestInterception(true);
-
-    page.on("request", (request) => {
-      if (
-        request.resourceType() === "image" ||
-        request.resourceType() === "stylesheet" ||
-        request.resourceType() === "font" ||
-        request.resourceType() === "texttrack" ||
-        request.resourceType() === "imageset" ||
-        request.resourceType() === "bacon" ||
-        request.resourceType() === "csp_report" ||
-        request.resourceType() === "object"
-      ) {
-        request.abort();
-      } else {
-        request.continue();
-      }
+    const req = await fetch(proxyUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        url,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
-    await page.setViewport({ width: 1920, height: 1080 });
-
-    await page.setUserAgent(
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-    );
-
-    await page.goto(url, {
-      waitUntil: "domcontentloaded",
-      timeout: 0,
-    });
-
-    await page.mouse.move(Math.random() * 1000, Math.random() * 1000);
-    await page.mouse.click(Math.random() * 1000, Math.random() * 1000);
-
-    const data = await page.evaluate(
-      () => document.querySelector("*").innerHTML
-    );
-
-    await page.close();
-    await browser.close();
+    const data = await req.text();
 
     return data;
   } catch (e) {
